28b79aa874c0fe733e013f10a49f9ac13ad8e0e2
belamoos <124020036+belamoos@users.noreply.github.com>
2023-07-17 20:00:49 -0400

    Initial commit

---
+++ src/test/java/com/arab/utilities/Driver.java
@@ -0,0 +1,19 @@
+package com.arab.utilities;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Driver {
+
+    private Driver() { }
+
+    private static WebDriver driver;
+
+    public static WebDriver getDriver(){
+        if (driver == null){
+         switch
+        }
+        return driver;
+    }
+
+}
